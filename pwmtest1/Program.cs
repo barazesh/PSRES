0b197c887f41cb14c96d47b6996324876baa2f85
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-09-29 18:20:13 +0330

    idk

--- pwmtest1/Program.cs
+++ pwmtest1/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO.Ports;
+
 
 namespace pwmtest1
 {
@@ -10,6 +12,28 @@ namespace pwmtest1
     {
         static void Main(string[] args)
         {
+            byte[] val = {0b0101_0101, 32 };
+            SerialPort newport = new SerialPort
+            {
+                PortName = "COM5",
+                BaudRate = 115200,
+                Parity = Parity.Even,
+                StopBits = StopBits.One,
+                DataBits = 8
+            };
+            newport.Open();
+            bool success = false;
+            byte light;
+            do
+            {
+                Console.WriteLine("Please insert an integer value between 0 to 100");
+                success = byte.TryParse(Console.ReadLine(), out light);
+                Console.WriteLine(Convert.ToString(light,2));
+                val[0] = light;
+                newport.Write(val, 0, 2);
+
+            } while (success);
+            
         }
     }
 }
