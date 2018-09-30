a8989f549697dd2dcc0d9d9967e36ec32acacd87
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-09-30 19:09:58 +0330

    adding class library

---
+++ PSRESLogic/Lamp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSRESLogic
+{
+    public class Lamp
+    {
+        public int currentIllumination { get; set; }
+        public int parent { get; set; }
+        public int zone { get; set; }
+        public int position { get; set; }
+    }
+}
