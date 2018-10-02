d20079d68949d4db2e5f5a77aa608c65ff1f3caf
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-10-02 14:25:47 +0330

    correcting the address in lamp class

--- PSRESLogic/Lamp.cs
+++ PSRESLogic/Lamp.cs
@@ -9,15 +9,19 @@ namespace PSRESLogic
     public class Lamp
     {
         public byte CurrentIllumination { get; set; }
-        public byte Parent { get; set; }
+        public int Parent { get; set; }
         public byte Zone { get; set; }
         public int Position { get; set; }
+        public int PWMpin { get; set; }
 
 
         public void Dim(byte dimValue)
         {
-            ushort address = Parent << 4;
-            string address = Convert.ToString(Zone, 2);
+            //prepare the date part for the message 
+            byte[] data = { 0, dimValue };
+            int address = Parent << 6;
+            address += (PWMpin << 3);
+            data[0] = (byte)address;
 
         }
     }
---
+++ consoletest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consoletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the parent value");
+            int parent = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(parent,2));
+            Console.WriteLine("enter pwm pin");
+            int pwmpin = int.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToString(pwmpin, 2));
+            int shiftedParent = parent << 6;
+            int shiftedPWM = pwmpin << 3;
+            Console.Write(shiftedParent);
+            Console.Write(" ");
+            Console.WriteLine(Convert.ToString(shiftedParent, 2));
+
+            Console.Write(shiftedPWM);
+            Console.Write(" ");
+            Console.WriteLine(Convert.ToString(shiftedPWM, 2));
+
+            Console.Write(shiftedParent+shiftedPWM);
+            Console.Write(" ");
+            Console.WriteLine(Convert.ToString(shiftedParent+shiftedPWM, 2));
+            Console.ReadKey();
+
+        }
+    }
+}
