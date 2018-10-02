2143d1d2150c2b683b336215dc798a393448feff
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-10-02 17:32:30 +0330

    adding comments to the Lamp class

--- PSRESLogic/Lamp.cs
+++ PSRESLogic/Lamp.cs
@@ -17,8 +17,11 @@ namespace PSRESLogic
 
         public void Dim(byte dimValue)
         {
-            //prepare the date part for the message 
-            byte[] data = { 0, dimValue };
+            //prepare the date part for the message that is going to be sent. the data part consists of two bytes. the first byte is the address of the receiving end and the second byte is the actual message.
+
+            byte[] data = { 0, dimValue };// the second byte is the argument being passed to the method which is the dutycycle of the PWM, controlling the illuminance of the lamp.
+
+            // the first byte which is the address consists of two parts: the most significant two bits indicate the parent code responsible for the lamp. the next three significant bits represent the PWM pin code responsible for the lamp
             int address = Parent << 6;
             address += (PWMpin << 3);
             data[0] = (byte)address;
