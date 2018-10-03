bca7e916790dabdb4080e0411364ee0fc3c62136
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-10-03 09:18:04 +0330

    adding parent class and the sensoring station class

---
+++ PSRESLogic/Parent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSRESLogic
+{
+    class Parent
+    {
+        public int Zone { get; set; }
+        public Lamp[] LampsUnderCover { get; set; }
+        public SensoringStation[] SensoringStationsUnderCover { get; set; }
+        public int Position { get; set; }
+    }
+}
