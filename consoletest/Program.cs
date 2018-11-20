21adf02a300a134914e29f3e4a787a33b9f22ae7
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-11-20 22:07:00 +0330

    trying to implement the auto-reader

--- consoletest/Program.cs
+++ consoletest/Program.cs
@@ -14,55 +14,88 @@ namespace consoletest
 {
     class Program
     {
+        static SerialPort myport = new SerialPort(SerialPort.GetPortNames()[2], 115200, Parity.None, 8, StopBits.One);
+
+
         static byte[] buffer = new byte[18];
         static Parent parent = new Parent(2);
         static SensorRecording[] sr = new SensorRecording[3];
-        static TimeDate timeDate = new TimeDate();
         static SensoringStation[] sensoringStations = new SensoringStation[3];
+
+
+        static TimeDate timeDate = new TimeDate();
+
         static Timer timer = new Timer(60000);
+
+        static Meter[] Meters = new Meter[2];
+
                           
         static void Main(string[] args)
         {
             timer.Elapsed += oneMinuteMark;
-          /* using (var context= new PSRESContext())
+
+            //instantiate the meters
+
+
+            Meters[0].Id = 1;
+            Meters[0].Serialcode = 205;
+            Meters[0].MeterDataReady += MeterDataHandler;
+
+            Meters[1].Id = 2;
+            Meters[1].Serialcode = 206;
+            Meters[1].MeterDataReady += MeterDataHandler;
+
+            parent.SensorDataReady += SensorDataHandler;
+
+
+
+            while (true)
             {
-                timeDate.year = DateTime.Now.Year;
-                timeDate.month= (byte)DateTime.Now.Month;
-                timeDate.day = (byte)DateTime.Now.Day;
-                timeDate.hour= (byte)DateTime.Now.Hour;
-                timeDate.minute = (byte)DateTime.Now.Minute;
-                context.Dates.Add(timeDate);
-
-                for (int i = 0; i < 3; i++)
+                //read meters
+                foreach (Meter m in Meters)
                 {
-                    sensoringStations[i] = new SensoringStation();
-                    sensoringStations[i].ParentId = 1;
-                    sensoringStations[i].ParentPin = i;
-                    sensoringStations[i].PositionId = 1;
-                    sensoringStations[i].Zone = 1;
-
+                    m.Read(myport);
                 }
-                context.SensoringStations.AddRange(sensoringStations);
-                context.SaveChanges();
 
-
-            }*/
-
-
-            SerialPort myport = new SerialPort(SerialPort.GetPortNames()[2], 115200, Parity.None, 8,StopBits.One);
-            myport.DataReceived += sensorDataRecievedHandler;
-            myport.Open();
-            Console.ReadKey();
-            myport.Close();
-
-            
+                //read sensors
+            }
 
 
         }
 
+        private static void SensorDataHandler(bool recived, SensorRecording[] recording)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void MeterDataHandler(bool recived, MeterRecording data)
+        {
+            throw new NotImplementedException();
+        }
+
         private static void oneMinuteMark(object sender, ElapsedEventArgs e)
         {
-            //save data
+            //add data to database
+
+            using (var context = new PSRESContext())
+            {
+
+                // add a new entry to timedate table 
+                timeDate.year = DateTime.Now.Year;
+                timeDate.month = (byte)DateTime.Now.Month;
+                timeDate.day = (byte)DateTime.Now.Day;
+                timeDate.hour = (byte)DateTime.Now.Hour;
+                timeDate.minute = (byte)DateTime.Now.Minute;
+                context.Dates.Add(timeDate);
+
+                // add sensor recordings
+
+
+                //add meter recordings
+
+                //save changes
+                context.SaveChanges();
+            }
         }
 
         private static void sensorDataRecievedHandler(object sender, SerialDataReceivedEventArgs e)
@@ -82,7 +115,6 @@ namespace consoletest
                 sr[i].Reliable = reliable;
                 File.AppendAllText(@"C:\Users\MRB\Documents\sensordata.csv", sr[i].ToString());
             }
-            addsensordata(sr);
 
             foreach (var b in buffer)
             {
@@ -94,14 +126,6 @@ namespace consoletest
 
             }
 
-        private static void addsensordata(SensorRecording[] sensorRecording)
-        {
-            using (var context=new PSRESContext())
-            {
-                context.SensorRecordings.AddRange(sensorRecording);
-                context.SaveChanges();
 
-            }
-        }
     }
 }
