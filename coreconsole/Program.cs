718c1f4d69205a7d27274eaef1b24edadeb75edc
Mohammadreza Barazesh <barazesh.mr90@gmail.com>
2018-12-10 08:54:58 +0330

    adding .NetCore Console app for reading the sensors

---
+++ coreconsole/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO.Ports;
+using PSRESLogic;
+
+
+namespace coreconsole
+{
+    class Program
+    {
+        static SerialPort SensorsPort = new SerialPort(SerialPort.GetPortNames()[2], 115200, Parity.None, 8, StopBits.One);
+        static byte[] buffer = new byte[18];
+
+        static Parent[] parents =
+        {
+            new Parent(1),
+            new Parent(2),
+            new Parent(3)
+        };
+        static bool datarecived;
+        static int parentindex;
+
+
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < parents.Length; i++)
+            {
+                parents[i].SensorDataReady += ShowSensrosData;
+
+            }
+            Console.WriteLine(SensorsPort.PortName);
+            Console.WriteLine("Press any key to begin");
+            Console.ReadKey();
+
+            while (true)
+            {
+                Console.WriteLine("Eneter the Parent Number to read Sensor data:");
+
+
+                SensorsPort.DataReceived -= parents[parentindex].sensorDataReceivedEventHandler;
+                parentindex = int.Parse(Console.ReadLine());
+                SensorsPort.DataReceived += parents[parentindex].sensorDataReceivedEventHandler;
+
+                parents[parentindex].readSensorData(SensorsPort);
+
+                Console.ReadKey();
+
+            }
+
+        }
+
+        private static void ShowSensrosData(bool recived)
+        {
+            Console.WriteLine();
+            if (SensorsPort.IsOpen)
+            {
+                SensorsPort.Close();
+            }
+            datarecived = recived;
+            if (recived)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Sensor Pack {i+1}");
+                    Console.WriteLine(parents[parentindex].sensorsdata[i].GetLatestData().ToString());
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("No Data Recieved");
+            }
+        }
+    }
+}
